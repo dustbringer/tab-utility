d92e5199655f4242f6b48a733ccb631f380fff05
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2022-08-28 08:19:22 +0100

    Replaced button with link, added reload on space

--- src/js/suspended.js
+++ src/js/suspended.js
@@ -1,6 +1,11 @@
-// https://stackoverflow.com/questions/39263292/how-does-the-great-suspender-work
-
 const urlParams = new URLSearchParams(window.location.search);
+
+// Reload on space bar
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    window.location.href = urlParams.get("url");
+  }
+})
 
 const timeElement = document.getElementById("suspended-time");
 const time = moment(urlParams.get("time"));
@@ -13,13 +18,17 @@ iconElement.setAttribute("src", urlParams.get("favicon"));
 
 const urlElement = document.getElementById("url");
 urlElement.appendChild(document.createTextNode(urlParams.get("url")));
+urlElement.href = urlParams.get("url");
 
 const titleElement = document.getElementById("title");
 titleElement.appendChild(document.createTextNode(urlParams.get("title")));
-document.title = `[S] ${urlParams.get("title")}`;
+document.title = `${urlParams.get("title")}`;
 
 const reload_button = document.getElementById("reload-button");
-reload_button.addEventListener("click", () => {
-  console.log(urlParams.get("url"));
-  window.location.href = urlParams.get("url");
-});
+reload_button.href = urlParams.get("url");
+// reload_button.addEventListener("click", () => {
+//   console.log(urlParams.get("url"));
+//   window.location.href = urlParams.get("url");
+// });
+
+
