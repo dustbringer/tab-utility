b46c2ebfc93c33e72d7882132a6cfe9a4fd1ea89
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2025-07-01 18:51:28 +1000

    Added favicon to suspended tabs

--- src-firefox/js/suspended.js
+++ src-firefox/js/suspended.js
@@ -7,13 +7,12 @@ document.addEventListener("keydown", (event) => {
   if (event.code === "Space") {
     window.location.href = urlParams.get("url");
   }
-})
+});
 
+// Set text
 const timeElement = document.getElementById("suspended-time");
 const time = Date.parse(urlParams.get("time"));
-timeElement.appendChild(
-  document.createTextNode(formatTime(time))
-);
+timeElement.appendChild(document.createTextNode(formatTime(time)));
 
 const iconElement = document.getElementById("icon");
 iconElement.setAttribute("src", urlParams.get("favicon"));
@@ -24,7 +23,7 @@ urlElement.href = urlParams.get("url");
 
 const titleElement = document.getElementById("title");
 titleElement.appendChild(document.createTextNode(urlParams.get("title")));
-document.title = `${urlParams.get("title")}`;
+document.title = `[s] ${urlParams.get("title")}`;
 
 const reload_button = document.getElementById("reload-button");
 reload_button.href = urlParams.get("url");
@@ -33,4 +32,13 @@ reload_button.href = urlParams.get("url");
 //   window.location.href = urlParams.get("url");
 // });
 
-
+// Set favicon
+if (urlParams.get("favicon")) {
+  let link = document.querySelector("link[rel~='icon']");
+  if (!link) {
+    link = document.createElement("link");
+    link.rel = "icon";
+    document.head.appendChild(link);
+  }
+  link.href = urlParams.get("favicon");
+}
